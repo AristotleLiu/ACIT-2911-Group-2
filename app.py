034d560c89bcd4c06afec0b3591c2bdb42ecb09e
Aris <AristotleLiu2021@gmail.com>
2023-05-02 14:00:54 -0700

    Adds the ability to update an animal's informtaion

--- app.py
+++ app.py
@@ -95,5 +95,48 @@ def delete_animal(animal_id):
     db.session.commit()
     return "Item deleted from the database"
 
+@app.route("/animal/<int:animal_id>", methods=["PUT"])
+def update_animal(animal_id):
+    data = request.json
+    
+    # Test to see the animal has all the required properties
+    for key in ["name", 
+                "age", 
+                "gender", 
+                "species", 
+                "price", 
+                "weight", 
+                "height", 
+                "health", 
+                "color", 
+                "purchase_date", 
+                "sold_date", 
+                "supplier", 
+                "purchase_price", 
+                "diet", 
+                "notes"]:
+        if key not in data:
+            return f"The JSON provided is invalid (missing: {key})", 400
+        
+    animal = db.session.get(Animal, animal_id)
+    animal.name = data["name"]
+    animal.age = data["age"]
+    animal.gender = data["gender"] 
+    animal.species = data["species"]
+    animal.price = data["price"] 
+    animal.weight = data["weight"] 
+    animal.height = data["height"]
+    animal.health = data["health"]
+    animal.color = data["color"]
+    animal.purchase_date = string_to_date(data["purchase_date"])
+    animal.sold_date = string_to_date(data["sold_date"])
+    animal.supplier = data["supplier"]
+    animal.purchase_price = data["purchase_price"]
+    animal.diet = json.dumps(data["diet"])
+    animal.notes = data["notes"]
+
+    db.session.commit()
+    return "Item updated in the database"
+
 if __name__ == "__main__":
     app.run(debug=True)
