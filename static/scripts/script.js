9b573b4747b972b951f4815443678275736e30d1
Riona <Riona.Wong1128@gmail.com>
2023-05-08 15:16:46 -0700

    added js for multiple animal fields in create invoice /// formatted create invoice page

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -69,3 +69,33 @@ const renderModal = async (element) => {
         console.error(error);
     }
 }
+
+// To add more Fields to the Invoice Creation page
+var animalCount = 1;
+  
+$(document).ready(function() {
+  $('.extra_animal_field').click(function() {
+
+    var newAnimal = $('.create_invoice_animal').last().clone();
+ 
+    var newClass = 'create_invoice_animal_' + animalCount;
+    newAnimal.removeClass().addClass(newClass);
+  
+    newAnimal.find('input').val('');
+ 
+    newAnimal.find('.extra_animal_field').remove();
+
+    var deleteButton = $('<a class="delete_animal_field" href="#">Delete Animal</a>');
+    deleteButton.data('target', '.' + newClass);
+    newAnimal.append(deleteButton);
+  
+    $('.add_invoice_animal').append(newAnimal);
+    animalCount++;
+  });
+  
+  $(document).on('click', '.delete_animal_field', function(e) {
+    var targetClass = $(this).data('target');
+    $(targetClass).remove();
+    e.preventDefault();
+  });
+});
