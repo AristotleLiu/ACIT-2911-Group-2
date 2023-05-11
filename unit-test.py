3106fd414273c550da4e30cec21311feae2a5622
Riona <Riona.Wong1128@gmail.com>
2023-05-11 11:50:39 -0700

    small change

--- unit-test.py
+++ unit-test.py
@@ -7,7 +7,7 @@ class Test_animal(unittest.TestCase):
     def test_date(self):
         date_format = '%Y-%m-%d'
         datetime_obj = datetime.strptime("2023-08-21", date_format)
-        self.assertEqual(dates.string_to_date("2023-08-21"),datetime_obj.date())
+        self.assertEqual(dates.string_to_date("2023-08-21"),datetime(year=2023,month=8,day=21).date())
 
 if __name__ == '__main__':
     unittest.main()
