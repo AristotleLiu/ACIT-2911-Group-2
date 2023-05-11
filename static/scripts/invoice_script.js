21c6fbfe19078df9323a9add097becf1cd9c7244
Aris <AristotleLiu2021@gmail.com>
2023-05-11 13:21:32 -0700

    Makes the view invoice button display all the invoice info except for the animals

--- static/scripts/invoice_script.js
+++ static/scripts/invoice_script.js
@@ -1,3 +1,29 @@
+const renderModal = async (element) => {
+  try {
+      const modalTarget = element.getAttribute("data-target");
+      // Get the invoice ID
+      const thread = element.parentNode.parentNode;
+      const invoiceId = thread.querySelector(".invoiceID").innerText;
+
+      // Make a GET request to the server to fetch the animal data
+      const response = await fetch(`/invoice/${invoiceId}`);
+      if (!response.ok) {
+          throw new Error('Failed to fetch invoice data');
+      }
+
+      // Parse the response JSON data and populate the modal form fields
+      const invoiceData = await response.json();
+      if (modalTarget == "#view-invoice-modal") {
+          document.querySelector(modalTarget + ' #invoice_name').innerHTML = `To: <span id="invoice_customer">${invoiceData["name"]}</span>`;
+          document.querySelector(modalTarget + ' #invoice_address').innerHTML = `${invoiceData["city"]}`;
+          document.querySelector(modalTarget + ' #invoice_location').innerHTML = `${invoiceData["province"]}`;
+          document.querySelector(modalTarget + ' #invoice_postal_code').innerHTML = `${invoiceData["postal_code"]}`;
+          document.querySelector(modalTarget + ' #invoice_phone').innerHTML = `${invoiceData["phone"]}`;
+      }
+  } catch (error) {
+      console.error(error);
+  }
+}
 
 // To add more Fields to the Invoice Creation page
 // ------------------------------------------------------------------leave for now 
