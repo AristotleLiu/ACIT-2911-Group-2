c55f700f71b9519c6f4aa6b16af1c7ad42ef6574
Timur R <timurreziapov2@gmail.com>
2023-05-19 09:55:10 -0700

    test fail

--- test_unit-test.py
+++ test_unit-test.py
@@ -40,7 +40,7 @@ class Test_animal(unittest.TestCase):
             'id': animal.id,
             'name': 'Fluffy',
             'age': 3,
-            'gender': 'female',
+            'gender': 'rfemale',
             'species': 'cat',
             'price': 50.0,
             'weight': 4.0,
